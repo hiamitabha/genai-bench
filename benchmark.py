ae1f4908d2d01760a6c5a6c3297938e231eb18e2
Amitabha Banerjee <hiamitabha@gmail.com>
2023-10-27 23:59:49 -0700

    Adding support for monitoring the service for multiple iterations with sleep in between

--- benchmark.py
+++ benchmark.py
@@ -1,10 +1,17 @@
 import requests
 import time
 import sys
+from datetime import datetime
 
 _TOGETHER_ENDPOINT = 'https://api.together.xyz/inference'
 
 def time_request(model, api_key):
+    """Issue a sample payload to the Together AI server for the specified
+       model and time the API response. Optionally returns the compute time reported by Together
+       if it was reported by the service
+       :param model Model name
+       :param api_key Together API Key
+    """
     payload = { 
         "model": model,
         "prompt": """\
@@ -34,17 +41,27 @@ def time_request(model, api_key):
     except requests.exceptions.RequestException as e:  # This is the correct syntax
         raise SystemExit(e)  
 
-def main(model, api_key):
-    (api_time, compute_time) = time_request(model,
-                                            api_key)
-    if api_time: 
-        print (f"Performance results for model {model}")
-        print (f"API end to end time in seconds {api_time:0.4f}")
-        print (f"Together reported compute time in seconds {compute_time:0.4f}")
+def test_api(model, api_key, iterations=100, sleep_in_seconds=300):
+    """Test a model for a specified number of iterations
+    :param model Model that needs to be tested
+    :param api_key Together API Key
+    :param iterations Total number of iterations for monitoring
+    :param sleep_in_seconds Seconds to sleep before next iteration of test
+    """
+    print (f"Performance results for model {model}")
+    print (f"Iteration #, Date/Time, API response time(s)")
+
+    for i in range(iterations):
+        now = datetime.now()
+        dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+        (api_time, compute_time) = time_request(model,
+                                                api_key)
+        print (f"{i},{dt_string},{api_time}")
+        time.sleep(sleep_in_seconds)
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print ("Usage: python3 benchmark.py <model> <api key> e.g.")
         print ("python3 benchmark.py togethercomputer/RedPajama-INCITE-7B-Instruct <api key from Together>")
         exit()
-    main(sys.argv[1], sys.argv[2])
+    test_api(sys.argv[1], sys.argv[2])
